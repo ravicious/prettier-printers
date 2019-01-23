34a4fce92b354e283a9b4d6e57f9aa942cb42a60
Rafał Cieślak <rafal.cieslak@lunarlogic.io>
2019-01-23 10:50:40 +0100

    Remove incorrect comment from JS example

--- example.js
+++ example.js
@@ -8,10 +8,6 @@ function fuzzy(buzzy) {
 }
 
 // Let's add a good color.
-// For things like arrays and objects, if they fit on one line they can stay
-// there, but we can also split them if we like.
-//
-// However, if they don't fit on one line, prettier always splits them.
 const colors = ['white', 'black', 'red', 'green', 'blue', 'yellow', 'carmine']
 
 // What if we want to parse RubyScript and use mungabunga?
